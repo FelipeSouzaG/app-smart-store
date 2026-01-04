540aa1d8b431da12571d71a01c13bb581863deb0
FelipeSouzaG <154008572+FelipeSouzaG@users.noreply.github.com>
2026-01-04 10:37:42 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 3001, // SmartStore Fixed Port
+    host: '0.0.0.0', // Mantido para acessibilidade
+    port: 3002,
+    strictPort: true,
+    // --- CORREÇÃO AQUI ---
+    allowedHosts: [
+      'smart-store.local.fluxoclean.com.br',
+      'host.docker.internal',
+    ],
+    // ---------------------
   },
 });
